45171c48400bc7bfa4d6aca7246040815e41946f
sebastian <treloxiii@gmail.com>
2024-03-04 17:02:41 -0500

    primer  Avance

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,50 @@
-console.log(tempData);
+
+    const movieContainer = document.getElementById("movie-container");
+  
+    
+    function mapDataToCards(data) {
+      return data.map((movie) => {
+        const card = document.createElement("div");
+        card.classList.add("movie-card");
+  
+        const title = document.createElement("h2");
+        title.textContent = movie.title;
+  
+        const year = document.createElement("p");
+        year.textContent = `Year: ${movie.year}`;
+  
+        const director = document.createElement("p");
+        director.textContent = `Director: ${movie.director}`;
+  
+        const duration = document.createElement("p");
+        duration.textContent = `Duration: ${movie.duration}`;
+  
+        const genre = document.createElement("p");
+        genre.textContent = `Genre: ${movie.genre.join(", ")}`;
+  
+        const rate = document.createElement("p");
+        rate.textContent = `Rate: ${movie.rate}`;
+  
+        
+        const poster = document.createElement("img");
+        poster.src = movie.poster;
+        poster.alt = movie.title;
+  
+        
+        card.appendChild(title);
+        card.appendChild(year);
+        card.appendChild(director);
+        card.appendChild(duration);
+        card.appendChild(genre);
+        card.appendChild(rate);
+        card.appendChild(poster);
+  
+        return card;
+      });
+    }
+  
+    const movieCards = mapDataToCards(tempData);
+  
+    
+    movieCards.forEach((card) => movieContainer.appendChild(card));
+;
