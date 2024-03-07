9de6d20893857ca3b8924e9d6e7e1b0fa174f444
sebastian <treloxiii@gmail.com>
2024-03-07 16:18:11 -0500

    Avance cuarto dia

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,24 +1,5 @@
 
-    const movieContainer = document.getElementById("movie-container");
-  
-    
-    function Cards(data) {
-      return data.map((movie) => {
-        const card = document.createElement("div");
-        card.classList.add("movie-card");
-        card.innerHTML = `
-        <img src="${movie.poster}" alt="${movie.title}" class="estiloimg">
-        <h2>${movie.title}</h2>
-        <p>Year: ${movie.year}</p>
-        <p>Director: ${movie.director}</p>
-        <p>Duration: ${movie.duration}</p>
-        <p>Genre: ${movie.genre.join(", ")}</p>
-        <p>Rate: ${movie.rate}</p>`;
-  
-        return card;
-      });
-      
-    }
+  const rendercards = require("./renderCards")
     
       $.get('https://students-api.2.us-1.fl0.io/movies', function(data){
           const movieCards = Cards(data);
---
+++ front/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports={
+    entry: './scripts/index.js',
+
+    output: {
+        path:__dirname + "/public",
+        filename: "bundle.js",
+
+    },
+
+};
